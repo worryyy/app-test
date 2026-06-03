c224e8d8ed13de15d5d5de192c44e4b6736d3667
worryyy <1536742833@qq.com>
2026-06-03 14:12:17 +0800

    revert

--- internal/topic/handler.go
+++ internal/topic/handler.go
@@ -23,12 +23,11 @@ func (h *Handler) Create(c *gin.Context) {
 		return
 	}
 
-	_, err := h.svc.Create(c.Request.Context(), middleware.GetClaims(c), &req)
+	data, err := h.svc.Create(c.Request.Context(), middleware.GetClaims(c), &req)
 	if err != nil {
 		responses.Fail(c, err)
-		return
-	
 	}
+	responses.Success.RespData(c, data)
 }
 
 func (h *Handler) Delete(c *gin.Context) {
